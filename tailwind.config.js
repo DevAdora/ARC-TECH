0260a2c1ffdf7bec9c20a964cc71d6e36304ab4d
Ar-ar <2r.reiyhes@gmail.com>
2024-10-03 22:07:00 +0800

    Wireframe for the output

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    backgroundColor: {
+      'gray-color': 'var(--gray-color)',
+      'warm-gray-color': 'var(--warm-gray-color)',
+      'warm-green-color': 'var(--warm-green-color)',
+      'pinkish-gray-color': 'var(--pinkish-gray-color)',
+      'pinkish-white-color': 'var(--pinkish-white-color)',
+    },
+    colors: {
+      'gray-color': 'var(--gray-color)',
+      'warm-gray-color': 'var(--warm-gray-color)',
+      'warm-green-color': 'var(--warm-green-color)',
+      'pinkish-gray-color': 'var(--pinkish-gray-color)',
+      'pinkish-white-color': 'var(--pinkish-white-color)',
+    }
   },
+  variants: [],
   plugins: [],
 };
