2dccee0e67aeac8ec9d7ead4633da217f67c1877
Ar-ar <2r.reiyhes@gmail.com>
2024-10-03 19:25:43 +0800

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html', // Adjust paths according to your project structure
+    './styles/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
