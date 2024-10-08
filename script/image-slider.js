4c2b185d4bd6c39f9900c90f07e6ba0dcd809f92
Ar-ar <2r.reiyhes@gmail.com>
2024-10-08 19:40:14 +0800

    fixed some errors

--- script/image-slider.js
+++ script/image-slider.js
@@ -1,16 +1,16 @@
 const slider = document.getElementById('slider');
 const nextButton = document.getElementById('next');
 let currentIndex = 0;
-const imagesPerSlide = 3; // Show 3 images at a time
+const imagesPerSlide = 3;
 const totalSlides = Math.ceil(document.querySelectorAll('.option').length / imagesPerSlide);
 
 nextButton.addEventListener('click', () => {
   currentIndex++;
 
   if (currentIndex === totalSlides) {
-    currentIndex = 0; // Loop back to the start
+    currentIndex = 0; 
   }
 
   // Calculate the distance to slide (currentIndex * 100% for each set of 3 images)
-  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+  slider.style.transform = `translateX(-${currentIndex * 25}%)`;
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: false,
   content: [
-    './*.html', // Adjust paths according to your project structure
+    './*.html', 
     './styles/*.css',
-  
+    './script/*.js',
   ],
   theme: {
-    extend: {},
-    backgroundColor: {
-      'gray-color': 'var(--gray-color)',
-      'warm-gray-color': 'var(--warm-gray-color)',
-      'warm-green-color': 'var(--warm-green-color)',
-      'pinkish-gray-color': 'var(--pinkish-gray-color)',
-      'pinkish-white-color': 'var(--pinkish-white-color)',
+    extend: {
+      backgroundColor: {
+        'gray-color': 'var(--gray-color)',
+        'warm-gray-color': 'var(--warm-gray-color)',
+        'warm-green-color': 'var(--warm-green-color)',
+        'pinkish-gray-color': 'var(--pinkish-gray-color)',
+        'pinkish-white-color': 'var(--pinkish-white-color)',
+      },
+      colors: {
+        'gray-color': 'var(--gray-color)',
+        'warm-gray-color': 'var(--warm-gray-color)',
+        'warm-green-color': 'var(--warm-green-color)',
+        'pinkish-gray-color': 'var(--pinkish-gray-color)',
+        'pinkish-white-color': 'var(--pinkish-white-color)',
+      },
     },
-    colors: {
-      'gray-color': 'var(--gray-color)',
-      'warm-gray-color': 'var(--warm-gray-color)',
-      'warm-green-color': 'var(--warm-green-color)',
-      'pinkish-gray-color': 'var(--pinkish-gray-color)',
-      'pinkish-white-color': 'var(--pinkish-white-color)',
-    }
   },
   variants: [],
   plugins: [],
